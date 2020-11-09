6904feaa04240b2c4f19dc7c82f6a6b9d535ca07
Administrador <midoiaga@gmail.com>
2020-11-09 11:33:14 +0100

    Puntuak kudeatuta

--- src/main/java/ehu/isad/controller/ui/BozkaketakKud.java
+++ src/main/java/ehu/isad/controller/ui/BozkaketakKud.java
@@ -62,90 +62,108 @@ public class BozkaketakKud implements Kudeatzaile {
 
     private int puntuak;
 
-    public void setHerrialde(String pHerrialde){
-        this.herrialde=pHerrialde;
-    }
-    public void setMainApp(Main mainapp){
-        this.mainapp=mainapp;
+    public void setHerrialde(String pHerrialde) {
+        this.herrialde = pHerrialde;
     }
 
-    public void setLbBozkatzaile(String pHerrialde){
-        lbBozkatzaile.setText(pHerrialde+"k horrela banatu nahi ditu bere puntuak");
+    public void setMainApp(Main mainapp) {
+        this.mainapp = mainapp;
+    }
+
+    public void setLbBozkatzaile(String pHerrialde) {
+        lbBozkatzaile.setText(pHerrialde + "k horrela banatu nahi ditu bere puntuak");
     }
 
     @FXML
     void onGorde(ActionEvent event) {
-        this.mainapp.top3Erakutsi();
+
+        int i = 0;
+        int puntuak = 0;
+        while (i < tbBotoak.getItems().size() && puntuak != 5) {
+            puntuak = puntuak + tbBotoak.getItems().get(i).getPuntuak();
+            i++;
+        }
+        if (puntuak == 5) {
+            this.sartuDB();
+            this.mainapp.top3Erakutsi();
+        }
     }
 
-    @Override
-    public void initialize(URL location, ResourceBundle resources) {
-        tbBotoak.setEditable(true);
 
-        ordezkapenModels.addAll(OrdezkapenDBKud.getInstantzia().ordezkpaenakLortu());
-
-        clHerrialdea.setCellValueFactory(new PropertyValueFactory<>("Herrialdea"));
-        clArtista.setCellValueFactory(new PropertyValueFactory<>("Artista"));
-        clAbestia.setCellValueFactory(new PropertyValueFactory<>("Abestia"));
-        clPuntuak.setCellValueFactory(new PropertyValueFactory<>("Puntuak"));
-        clBandera.setCellValueFactory(new PropertyValueFactory<OrdezkapenModel, Image>("image"));
-
-        clPuntuak.setCellFactory(
-                TextFieldTableCell.forTableColumn(new IntegerStringConverter()));
-        //Callback<TableColumn<OrdezkapenModel, String>, TableCell<OrdezkapenModel, String>> defaultTextFieldCellFactory = TextFieldTableCell.<OrdezkapenModel>forTableColumn();
-        clPuntuak.setOnEditCommit(
-                t -> {
-                   t.getTableView().getItems().get(t.getTablePosition().getRow()).setPuntuak(t.getNewValue());
-                   int punt=t.getTableView().getItems().get(t.getTablePosition().getRow()).getPuntuak();
-                   String bozkatua=t.getTableView().getItems().get(t.getTablePosition().getRow()).getHerrialdea();
-                   BozkaketaDBKud.getInstantzia().bozkaketaSartu(punt,this.herrialde,bozkatua);
-                   OrdezkapenDBKud.getInstantzia().bozkaketakEguneratu(punt,bozkatua);
-                });
-
-        clBandera.setCellFactory(p -> new TableCell<>() {
-            public void updateItem(Image image, boolean empty) {
-                if (image != null && !empty){
-                    final ImageView imageview = new ImageView();
-//                  imageview.setFitHeight(50);
-//                  imageview.setFitWidth(50);
-                    imageview.setImage(image);
-                    setGraphic(imageview);
-                    setAlignment(Pos.CENTER);
-                    // tbData.refresh();
-                }else{
-                    setGraphic(null);
-                    setText(null);
-                }
-            };
-        });
-
-
-        Callback<TableColumn<OrdezkapenModel, Integer>, TableCell<OrdezkapenModel, Integer>> defaultTextFieldCellFactory
-                = TextFieldTableCell.forTableColumn(new IntegerStringConverter());
-
-        clPuntuak.setCellFactory(col -> {
-            TableCell<OrdezkapenModel, Integer> cell = defaultTextFieldCellFactory.call(col);
-
-
-        cell.setOnMouseClicked(event -> {
-            if (! cell.isEmpty()) {
-                if (cell.getTableView().getSelectionModel().getSelectedItem().getHerrialdea().equals(this.herrialde)) {
-                    cell.setEditable(false);
-                }else {
-                    cell.setEditable(true);
+        private void sartuDB () {
+            int i;
+            for (i = 0; i<tbBotoak.getItems().size(); i++) {
+                if (tbBotoak.getItems().get(i).getPuntuak() !=0) {
+                    int punt = tbBotoak.getItems().get(i).getPuntuak();
+                    String bozkatua = tbBotoak.getItems().get(i).getHerrialdea();
+                    BozkaketaDBKud.getInstantzia().bozkaketaSartu(punt, this.herrialde, bozkatua);
+                    OrdezkapenDBKud.getInstantzia().bozkaketakEguneratu(punt, bozkatua);
                 }
             }
-        });
+        }
+        @Override
+        public void initialize (URL location, ResourceBundle resources){
+            tbBotoak.setEditable(true);
 
-        return cell ;
-    });
+            ordezkapenModels.addAll(OrdezkapenDBKud.getInstantzia().ordezkpaenakLortu());
 
-        // modeloaren datuak taulan txertatu
-        tbBotoak.setItems(ordezkapenModels);
+            clHerrialdea.setCellValueFactory(new PropertyValueFactory<>("Herrialdea"));
+            clArtista.setCellValueFactory(new PropertyValueFactory<>("Artista"));
+            clAbestia.setCellValueFactory(new PropertyValueFactory<>("Abestia"));
+            clPuntuak.setCellValueFactory(new PropertyValueFactory<>("Puntuak"));
+            clBandera.setCellValueFactory(new PropertyValueFactory<OrdezkapenModel, Image>("image"));
+
+//            clPuntuak.setCellFactory(
+//                    TextFieldTableCell.forTableColumn(new IntegerStringConverter()));
+            clPuntuak.setOnEditCommit(
+                    t -> {
+                        t.getTableView().getItems().get(t.getTablePosition().getRow()).setPuntuak(t.getNewValue());
+
+                    });
+
+            clBandera.setCellFactory(p -> new TableCell<>() {
+                public void updateItem(Image image, boolean empty) {
+                    if (image != null && !empty) {
+                        final ImageView imageview = new ImageView();
+//                  imageview.setFitHeight(50);
+//                  imageview.setFitWidth(50);
+                        imageview.setImage(image);
+                        setGraphic(imageview);
+                        setAlignment(Pos.CENTER);
+                        // tbData.refresh();
+                    } else {
+                        setGraphic(null);
+                        setText(null);
+                    }
+                }
+
+                ;
+            });
 
 
+            Callback<TableColumn<OrdezkapenModel, Integer>, TableCell<OrdezkapenModel, Integer>> defaultTextFieldCellFactory
+                    = TextFieldTableCell.forTableColumn(new IntegerStringConverter());
+
+            clPuntuak.setCellFactory(col -> {
+                TableCell<OrdezkapenModel, Integer> cell = defaultTextFieldCellFactory.call(col);
 
 
+                cell.setOnMouseClicked(e -> {
+                    if (!cell.isEmpty()) {
+                        if (cell.getTableView().getSelectionModel().getSelectedItem().getHerrialdea().equals(this.herrialde)) {
+                            cell.setEditable(false);
+                        } else {
+                            cell.setEditable(true);
+                        }
+                    }
+                });
 
+                return cell;
+            });
+
+            // modeloaren datuak taulan txertatu
+            tbBotoak.setItems(ordezkapenModels);
+
+
+        }
     }
-}
